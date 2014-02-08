a8b772ee1dab7e93489813f612ca56ae01ff3f97
iduhetonas <tyler.huffman@tylerh.org>
2014-02-08 15:38:44 -0700

    Finished first iteration and test of the Fletcher's checksum

--- daily-programmer/checksum/main.hs
+++ daily-programmer/checksum/main.hs
@@ -13,26 +13,27 @@ Portability :  portable
 import Data.ByteString as B
 import Data.Bits
 import Data.Word
-import Control.Monad.State
 
 
+-- 
 checksumWord :: [Word8] -> Word16
 checksumWord bytes = 
-  let (c0, c1) = checksumWord' Prelude.foldr (\acc x -> acc + x `mod` 255) 0 bytes
-  in  bytePack c0 c1
+  let result = Prelude.foldl addValues (0,0) bytes
+  in  bytePack result
 
-checksumWord' :: State ->
-checksumWord' [] = []
-checksumWord' (x:xs) = (x + checksumWord' xs, 
+addValues :: (Word8, Word8) -> Word8 -> (Word8, Word8)
+addValues (c0, c1) byte = 
+  let r0 = c0 + byte `mod` 255
+      r1 = c1 + r0 `mod` 255
+  in (r0, r1)
 
-bytePack :: Word8 -> Word8 -> Word16
-bytePack byte1 byte2 = (toWord16 byte1 .|. (shift (toWord16 byte2) 8)) :: Word16
+bytePack :: (Word8, Word8) -> Word16
+bytePack (byte1, byte2) = (toWord16 byte1 .|. (shift (toWord16 byte2) 8)) :: Word16
 
 toWord16 :: Word8 -> Word16
 toWord16 byte = fromIntegral byte 
 
-
-
+main :: IO ()
 main = print $ checksumWord [1, 2]
   
   
