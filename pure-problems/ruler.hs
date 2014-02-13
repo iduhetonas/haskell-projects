f1b47825c086982735c9e178ddd15fc202937d41
iduhetonas <tyler.huffman@tylerh.org>
2014-02-12 21:12:01 -0700

    Finished ruler.hs problem

--- pure-problems/ruler.hs
+++ pure-problems/ruler.hs
@@ -9,3 +9,19 @@
 -- 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4, . . .
 -- where the nth element in the stream (assuming the first element
 -- corresponds to n = 1) is the largest power of 2 which evenly divides n
+
+
+-- The stream part is boring, decided to go
+-- simple and pure instead
+ruler :: (Integral a) => [a] -> [a]
+ruler [] = []
+ruler (x:xs) = (powerTwo x) : ruler xs
+
+powerTwo :: (Integral a) => a -> a
+powerTwo num = if (num `mod` 2) /= 0
+               then 0
+               else powerTwo (num `div` 2) + 1
+
+main = do
+  return $ ruler [1..20000]
+
