e8db51dfb2b9a48d2c63088a0a74b4e3771898bf
iduhetonas <tyler.huffman@tylerh.org>
2014-02-14 23:10:56 -0700

    Finished the next problem

--- haskell-99-problems/main.hs
+++ haskell-99-problems/main.hs
@@ -2,7 +2,8 @@ module Main (main) where
 import H99Prob 
 
 main = do
-  print . isPalindrome $ "The pig thought he could fly"
-  print . isPalindrome $ "racecar"
-  print . isPalindrome $ "Racecar"
-  print . flatten $ (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+  --print . isPalindrome $ "The pig thought he could fly"
+  --print . isPalindrome $ "racecar"
+  --print . isPalindrome $ "Racecar"
+  --print . flatten $ (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+  print . compress $ "kkkkkkkkkkkkkkaaaaaaayyyyyyyyyyyyyyyyyylllllllllaaaaaaaaaaaaaaaaa"
