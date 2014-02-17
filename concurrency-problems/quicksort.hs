099238722f63f3193c0290f23f028b81d2caa614
Tyler Huffman <tyler.huffman@tylerh.org>
2014-02-17 16:21:17 -0700

    Added some comments to work on later for the quicksort problem

---
+++ concurrency-problems/quicksort.hs
@@ -0,0 +1,18 @@
+module TylerhConcurrency 
+  where
+{- Implementation of quicksort from memory.
+ -
+ - Hoping to extend this into a concurrent program that I 
+ - attempted in C a long time ago.
+ -}
+
+-- Steps to quicksort:
+-- 1) Take the first element as the "midpoint"
+-- 2) Put >= into first list, < into second
+-- 3) Call recursively and join the lists, i.e. 
+-- (quicksort list1) ++ (quicksort list2)
+--
+-- *) When it works correctly, run each recursive call as a separate thread
+
+--quicksort :: (Ord a) => [a] -> [a]
+--quicksort list = 
