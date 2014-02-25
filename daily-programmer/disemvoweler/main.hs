614c5e9626f0828096aaefb73c9af9de882eac81
iduhetonas <tyler.huffman@tylerh.org>
2014-02-24 20:21:07 -0700

    Changed a few things to make it slightly more readable

--- daily-programmer/disemvoweler/main.hs
+++ daily-programmer/disemvoweler/main.hs
@@ -1,15 +1,17 @@
+{- This function reads input.txt, and separates the vowels from the consonants 
+ - and prints them both
+ -}
+
 main = do
   str <- readFile "input.txt"
   (consonants, vowels) <- splitVowels str
   print $ reverse consonants
   print $ reverse vowels
 
-
 splitVowels :: String -> IO (String, String)
 splitVowels path = return $ foldl _splitVowels ([],[]) path
 
 _splitVowels :: (String, String) -> Char -> (String, String)
-_splitVowels (consonants, vowels) inVal | vowel inVal = (consonants, (inVal : vowels))
-                                        | otherwise = ((inVal : consonants), vowels)
-
-vowel c = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+_splitVowels (consonants, vowels) inVal 
+  | (`elem` "aeiou") inVal = (consonants, (inVal : vowels))
+  | otherwise = ((inVal : consonants), vowels)
