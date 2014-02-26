02c7882bf3c0cff42ec15f5c92d7de3465ce31a9
iduhetonas <tyler.huffman@tylerh.org>
2014-02-25 19:05:04 -0700

    Changed GUI to accept bindings

--- gui-tools/main.hs
+++ gui-tools/main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
+import System.Glib.Signals
+import System.Exit
 
 main = do
     initGUI
@@ -10,10 +12,17 @@ main = do
 
     mainWindow <- builderGetObject builder castToWindow "mainwindow"
     --onDestroy mainWindow mainQuit
+    
 
-    helloWorldButton <- builderGetObject builder castToButton "button1"
-    --onClicked helloWorldButton (putStrLn "Hello World")
+    button1 <- builderGetObject builder castToButton "button1"
+    on button1 buttonActivated $ do
+      putStrLn "Hello, world!"
+
+    button2 <- builderGetObject builder castToButton "button2"
+    on button2 buttonActivated $ do
+      exitWith ExitSuccess
+      
+    --buttonActivated (on button1ko
 
     widgetShowAll mainWindow
     mainGUI
-
